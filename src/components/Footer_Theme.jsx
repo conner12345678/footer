0c690c9c4c01e1cba5c753192439635565ffe74b
conner12345678 <cehly065@west-mec.org>
2024-11-22 14:37:21 -0700

    added styling and a line for stuff on footer

--- src/components/Footer_Theme.jsx
+++ src/components/Footer_Theme.jsx
@@ -1,13 +1,16 @@
+import '../css/Footer_Theme.css'
+
 const Footer_Theme = ({logo, about, socialLinks}) => {
   const keys = Object.keys(socialLinks)
   const values = Object.values(socialLinks)
   return (
     <div className="ftr">
       <img src={logo} alt="Logo" className="lgo" />
-      <p className='about'>{about}</p>
       {keys.map((key) => (
-        <a href={socialLinks[key].theLink}>{socialLinks[key].theLogo}</a>
+        <a className="links" href={socialLinks[key].theLink}>{socialLinks[key].theLogo}</a>
       ))}
+      <hr className='line'/>
+      <p className='about'>{about}</p>
     </div>
   )
 }
